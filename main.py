319331e15242cd52891b5bc2ba0a68f63fa4e565
PathaSnehith <pathasnehith@gmail.com>
2025-09-05 13:50:21 +0530

    create file name

--- main.py
+++ main.py
@@ -12,4 +12,26 @@ def gcd(a, b):
     """
     while b:
         a, b = b, a % b
-    return a
+    return a 
+
+# ...existing code...
+# 22bcs081 (Prime Number)
+def is_prime(n):
+    """
+    Check if a number is prime
+    Author: 22bcs081
+    """
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
