4d9c7a4364460037eb07c3502d796399e9f435fa
PathaSnehith <pathasnehith@gmail.com>
2025-09-05 14:00:24 +0530

    Added test_prime and modified main.py

--- main.py
+++ main.py
@@ -13,3 +13,21 @@ def gcd(a, b):
     while b:
         a, b = b, a % b
     return a
+# 22bcs081(Factorial)
+def factorial(n):
+    """
+    Factorial function
+    Author
+    :param n: non-negative integer
+    :return: n!
+    """
+    if not isinstance(n, int):
+        raise TypeError("Input must be a non-negative integer.")
+    if n < 0:
+        raise ValueError("Input must be a non-negative integer.")
+    if n == 0:
+        return 1
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
